be816ba758da69c0215eca222b2e9403724cb803
JinnyHui <hui@iupui.edu>
2018-11-29 19:29:23 -0500

    last queation left

--- JH_HW4_Eclat.py
+++ JH_HW4_Eclat.py
@@ -10,24 +10,196 @@ import csv
 import sys
 
 
+# def AssociationRule(rule_list, confi):
+#     """
+#     generate all the association
+#     :param rule_list:
+#     :param confi:
+#     :return:
+#     """
+#     return association_list
+
 ###################################################
 # convert the dataset into an itemset dataset
-f = open('house-votes-84.data', 'r')
-o = open('converted.txt', 'w')
-lines = f.readlines()
+# f = open('house-votes-84.data', 'r')
+# o = open('converted.tab', 'w')
+# lines = f.readlines()
+# for line in lines:
+#     strpline = line.rstrip()
+#     arr = strpline.split(',')
+#     newline = []
+#     for i in range(len(arr)):
+#         if arr[i] == 'y':
+#             newline.append(str(i))
+#     if arr[0] == 'republican':
+#         newline.append(str(100))
+#     else:
+#         newline.append(str(200))
+#     print(*newline, sep=' ')
+#     myline = '\t'.join(newline)
+#     o.write(myline + '\n')
+# o.close()
+
+###################################################
+# run the eclat algorithm with 20% support with descending support
+# './eclat -s20 converted.tab freq.out'
+# generate association rule
+# './eclat -tr -s20 -v" (%c)" converted.tab association.out
+
+###################################################
+# read in the frequent set file, load it into a dict
+file_in = open('freq.out', 'r')
+lines = file_in.readlines()
+item_dict = {}
 for line in lines:
     strpline = line.rstrip()
-    arr = strpline.split(',')
-    newline = []
-    for i in range(len(arr)):
-        if arr[i] == 'y':
-            newline.append(str(i))
-    if arr[0] == 'republican':
-        newline.append(str(100))
-    else:
-        newline.append(str(200))
-    print(*newline, sep=',')
-    myline = '\t'.join(newline)
-    o.write(myline + '\n')
-o.close()
+    arr = strpline.split(' ')
+    key_tuple = tuple(map(int, arr[:-1]))
+    item_dict[key_tuple] = int(arr[-1].strip('()'))
+file_out = open('Q2_report.txt', 'w')
 
+###################################################
+# a. Run the itemset mining algorithm with 20% support. How many frequent itemsets are there
+ans_a = "Q2a: The number of frequent itemsets is: " + str(len(item_dict))
+print(ans_a)
+file_out.write(ans_a + '\n')
+
+###################################################
+# b. Write top 10 itemsets (in terms of highest support value)
+top_10 = sorted(item_dict.items(), key=lambda x: -x[1])[:10]
+counter = 1
+file_out.write('\nQ2b: Top 10 itemsets are:\n')
+print('\nQ2b: Top 10 itemsets are:')
+for item in top_10:
+    freq_set, freq = item
+    ans_b = 'Frequent set NO.%-3s:%-10s Frequency: %s' % (str(counter), str(freq_set), str(freq))
+    print(ans_b)
+    file_out.write(ans_b + '\n')
+    counter += 1
+
+###################################################
+# c. How many frequent itemsets have 100 as part of itemsets
+# print('\nQ2c: Frequent itemsets have 100 are:')
+counter_100 = 0
+for key, value in item_dict.items():
+    if 100 in key:
+        counter_100 += 1
+        # print(key)
+ans_c = '\nQ2c: The number of frequent itemsets have 100 is: ' + str(counter_100)
+print(ans_c)
+file_out.write(ans_c)
+
+###################################################
+# d. How many frequent itemsets have 200 as part of itemsets
+# print('\nQ2d: Frequent itemsets have 200 are:')
+counter_200 = 0
+for key, value in item_dict.items():
+    if 200 in key:
+        counter_200 += 1
+        # print(key)
+ans_d = '\nQ2d: The number of frequent itemsets have 200 is: ' + str(counter_200)
+print(ans_d)
+file_out.write('\n' + ans_d)
+
+###################################################
+# read in the association rule file, store into a list
+file_rule = open('association.out', 'r')
+lines = file_rule.readlines()
+rule_dict = {}
+for line in lines:
+    strpline = line.rstrip()
+    arr = strpline.split(' ')
+    arr[-1] = float("{0:.3f}".format(float(arr[-1].strip('()'))))
+    body_tuple = tuple(arr[2:-1])
+    key_tuple = (int(arr[0]), body_tuple)
+    value = arr[-1]
+    rule_dict[key_tuple] = value
+
+###################################################
+# construct dictionary of rules with head 100, 200
+dict_100 = {}
+dict_200 = {}
+for key, value in rule_dict.items():
+    if key[0] == 100:
+        dict_100[key] = value
+    elif key[0] == 200:
+        dict_200[key] = value
+
+###################################################
+# e. Write top 10 association rules where the rule’s head is 100
+print('\nQ2e: Top 10 association rules where the rule’s head is 100: ')
+file_out.write('\n' + '\nQ2e: Top 10 association rules where the rule’s head is 100:')
+top_10_100 = sorted(dict_100.items(), key=lambda x: -x[1])[:10]
+count_100_rule = 0
+for i in top_10_100:
+    # print(i)
+    count_100_rule += 1
+    rule, confi = i
+    body = ', '.join(list(rule[1]))
+    rule_string = str(rule[0]) + ' <- ' + body
+    ans_e = 'Rule NO.%-3s: %-26s Confidence: %s' % (str(count_100_rule), rule_string, str(confi))
+    print(ans_e)
+    file_out.write('\n' + ans_e)
+
+###################################################
+# f. List rules with head 100 which the confidence value is more than 75%
+print('\nQ2f: Rules with head 100 with confidence higher than 75% are: ')
+file_out.write('\n\nQ2f: Rules with head 100 with confidence higher than 75% are: ')
+rule_100_75 = {}
+count_100_75 = 0
+for key, value in dict_100.items():
+    if value > 0.75:
+        count_100_75 += 1
+        body = ', '.join(list(key[1]))
+        rule_string = str(key[0]) + ' <- ' + body
+        ans_f = 'Rule NO.%-3s: %-26s Confidence: %s' % (str(count_100_75), rule_string, str(value))
+        print(ans_f)
+        file_out.write('\n' + ans_f)
+print('Total number is:', str(count_100_75))
+file_out.write('\nTotal number is:' + str(count_100_75))
+
+###################################################
+# g. Write top 10 association rules where the rule’s head is 200
+print('\nQ2g: Top 10 association rules where the rule’s head is 200: ')
+file_out.write('\n\nQ2g: Top 10 association rules where the rule’s head is 200:')
+top_10_200 = sorted(dict_200.items(), key=lambda x: -x[1])[:10]
+count_200_rule = 0
+for i in top_10_200:
+    # print(i)
+    count_200_rule += 1
+    rule, confi = i
+    body = ', '.join(list(rule[1]))
+    rule_string = str(rule[0]) + ' <- ' + body
+    ans_g = 'Rule NO.%-3s: %-26s Confidence: %s' % (str(count_200_rule), rule_string, str(confi))
+    print(ans_g)
+    file_out.write('\n' + ans_g)
+
+###################################################
+# h. List rules with head 200 which the confidence value is more than 75%
+print('\nQ2h: Rules with head 200 with confidence higher than 75% are: ')
+file_out.write('\n\nQ2h: Rules with head 200 with confidence higher than 75% are: ')
+rule_200_75 = {}
+count_200_75 = 0
+for key, value in dict_200.items():
+    if value > 0.75:
+        count_200_75 += 1
+        body = ', '.join(list(key[1]))
+        rule_string = str(key[0]) + ' <- ' + body
+        ans_h = 'Rule NO.%-3s: %-26s Confidence: %s' % (str(count_200_75), rule_string, str(value))
+        print(ans_h)
+        file_out.write('\n' + ans_h)
+    # else:
+    #     print('There is a 75%')
+print('Total number is:', str(count_200_75))
+file_out.write('\nTotal number is:' + str(count_200_75))
+
+###################################################
+# construct the new dataset
+
+
+###################################################
+# i. soft-margin SVM on rules with more than 75% confidence
+
+
+
+file_out.close()
--- JH_HW4_SVM.py
+++ JH_HW4_SVM.py
@@ -24,47 +24,42 @@ df.replace('?', 0, inplace=True)
 # split dataset into 1:3 for tuning and 3—fold respectively
 X = np.array(df.loc[:, 1:])
 y = np.array(df.loc[:, 0])
-X_tune, X_3_fold, y_tune, y_3_fold = train_test_split(X, y, stratify=y, test_size=0.75)  # test_size stands for 3-folds
+X_tune, X_3_fold, y_tune, y_3_fold = train_test_split(X, y, stratify=y, test_size=0.75, random_state=100)  # test_size stands for 3-folds
 
 ###################################################
-# tune the parameters
-X_tune_train, X_tune_test, y_tune_train, y_tune_test = train_test_split(X_tune, y_tune, stratify=y_tune, test_size=0.30)
-
-# tuning with linear svm
-# c_list_linear = [100, 10, 1, 0.5, 0.4, 0.3, 0.2, 0.1, 0.001]
-# for i in c_list_linear:
-#     clf_linear = svm.SVC(kernel='linear', C=i).fit(X_tune_train, y_tune_train)
-#     score = clf_linear.score(X_tune_test, y_tune_test)
-#     print('C =', str(i), ', score:', str(score))
-# print('After several runs, when C=0.1, outputs best score overall.')
-
-# tuning with gaussian svm
-# c_list_gaussian = [70, 60, 50, 10, 8, 5, 3, 1, 0.5, 0.1]
-# gamma_list = [0.1, 0.01, 0.001, 0.0001]
-# for i in c_list_gaussian:
-#     clf_gaussian = svm.SVC(kernel='rbf', C=i, gamma=0.01).fit(X_tune_train, y_tune_train)
-#     score = clf_gaussian.score(X_tune_test, y_tune_test)
-#     print('C =', str(i), ' score:', str(score))
-    # for j in gamma_list:
-    #     clf_gaussian = svm.SVC(kernel='rbf', C=i, gamma=j).fit(X_tune_train, y_tune_train)
-    #     score = clf_gaussian.score(X_tune_test, y_tune_test)
-    #     print('C =', str(i), 'gamma =', str(j), ' score:', str(score))
+# set the tuning parameters
+c_list_linear = [100, 10, 1, 0.5, 0.4, 0.3, 0.2, 0.1, 0.001]
+c_list_gaussian = [70, 60, 50, 10, 8, 5, 3, 1, 0.5, 0.1]
+gamma_list_gaussian = [0.1, 0.01, 0.001, 0.0001]
 
 ###################################################
 # split 3_fold into 3 folds with StratifiedKFold
-skf = StratifiedKFold(n_splits=3)
-# X_train = []
-# X_test = []
-# y_train = []
-# y_test = []
+skf = StratifiedKFold(n_splits=3, random_state=100)
+C_linear_list = [0.1, 0.1, 0.1]
+C_gaussian_list = [5, 8, 10]
+gamma_list = [0.1, 0.01, 0.1]
 precision_list_linear = []
 precision_list_gaussian = []
 counter = 1
 for train_index, test_index in skf.split(X_3_fold, y_3_fold):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
-    clf_linear = svm.SVC(kernel='linear', C=0.1).fit(X_train, y_train)
-    clf_gaussian = svm.SVC(kernel='rbf', C=5, gamma=0.01).fit(X_train, y_train)
+
+    # linear parameter tuning
+    # for i in c_list_linear:
+    #     clf_linear = svm.SVC(kernel='linear', C=i).fit(X_train, y_train)
+    #     score = clf_linear.score(X_tune, y_tune)
+    #     print('C =', str(i), ', score:', str(score))
+    clf_linear = svm.SVC(kernel='linear', C=C_linear_list[counter-1]).fit(X_train, y_train)
+
+    # Gaussian parameter tuning
+    # for i in c_list_gaussian:
+    #     for j in gamma_list_gaussian:
+    #         clf_gaussian = svm.SVC(kernel='rbf', C=i, gamma=j).fit(X_train, y_train)
+    #         score = clf_gaussian.score(X_tune, y_tune)
+    #         print('C =', str(i), 'gamma =', str(j), 'score:', str(score))
+
+    clf_gaussian = svm.SVC(kernel='rbf', C=C_gaussian_list[counter-1], gamma=gamma_list[counter-1]).fit(X_train, y_train)
 
     # calculate the accuracy for linear model
     y_linear_pred = pd.Series(clf_linear.predict(X_test), name='Prediction')
@@ -83,24 +78,37 @@ for train_index, test_index in skf.split(X_3_fold, y_3_fold):
     n_gaussian = nii_gaussian + df_confusion_gaussian.iloc[0][1] + df_confusion_gaussian.iloc[1][0]
     precision_gaussian = nii_gaussian/n_gaussian
     precision_list_gaussian.append(precision_gaussian)
-    print('###########################################################')
-    print('The', str(counter), 'fold evaluation:')
+    print('################################')
+    print('    The', str(counter), 'fold evaluation')
     print('Linear SVM evaluation:                     ')
     print(df_confusion_linear)
-    print('precision =', str(precision_linear), '\n')
+    print('Best parameter C = ', str(C_linear_list[counter-1]))
+    print('precision =', str("{:.3f}".format(precision_linear)), '\n')
     print('Gaussian SVM evaluation:                   ')
     print(df_confusion_gaussian)
-    print('precision =', str(precision_gaussian), '\n')
+    print('Best parameter C = ', str(C_gaussian_list[counter - 1]), 'gamma =', str(gamma_list[counter-1]))
+    print('precision =', str("{:.3f}".format(precision_gaussian)), '\n')
     counter += 1
 
-print('###########################################################')
-print('#              Linear SVM evaluation report               #')
-print('###########################################################')
-print('Average accuracy:', str("{:.2f}".format(mean(precision_list_linear))))
-print('Standard deviation:', str("{:.2f}".format(stdev(precision_list_linear))), '\n')
+report_out = open('Q1_report.txt', 'w')
+separator = '###########################################################'
+avg_acc_linear_str = str("{:.3f}".format(mean(precision_list_linear)))
+avg_dev_linear_str = str("{:.3f}".format(stdev(precision_list_linear)))
+print(separator + '\n#              Linear SVM evaluation report               #' + '\n' + separator)
+print('Average accuracy:' + avg_acc_linear_str)
+print('Standard deviation:' + avg_dev_linear_str + '\n')
+avg_acc_gaussian_str = str("{:.3f}".format(mean(precision_list_gaussian)))
+avg_dev_gaussian_str = str("{:.3f}".format(stdev(precision_list_gaussian)))
+print(separator + '\n#              Gaussian SVM evaluation report             #' + '\n' + separator)
+print('Average accuracy:' + avg_acc_gaussian_str)
+print('Standard deviation:' + avg_dev_gaussian_str + '\n')
 
-print('###########################################################')
-print('#              Gaussian SVM evaluation report             #')
-print('###########################################################')
-print('Average accuracy:', str("{:.2f}".format(mean(precision_list_gaussian))))
-print('Standard deviation:', str("{:.2f}".format(stdev(precision_list_gaussian))), '\n')
+# output to txt
+report_out.write(separator + '\n#              Linear SVM evaluation report               #' + '\n' + separator)
+report_out.write('\nAverage accuracy:' + avg_acc_linear_str)
+report_out.write('\nStandard deviation:' + avg_dev_linear_str)
+report_out.write(separator + '\n#              Gaussian SVM evaluation report             #' + '\n' + separator)
+report_out.write('\nAverage accuracy:' + avg_acc_gaussian_str)
+report_out.write('\nStandard deviation:' + avg_dev_gaussian_str)
+
+report_out.close()
